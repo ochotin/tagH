2389d794f0d7fd11394d312f35e7208c76491556
Olivier Chotin <olivier.chotin@ocia.fr>
2022-07-04 17:24:59 +0200

    Signed-off-by: Olivier Chotin <olivier.chotin@ocia.fr>

--- app.py
+++ app.py
@@ -8,12 +8,8 @@ import re
 # from deep_translator import GoogleTranslator
 # import spacy
 #import en_core_web_sm
-# import nltk
-#nltk.download('punkt')
-# nltk.download('wordnet')
-# nltk.download('stopwords')
-#nltk.download('all') 
-# from nltk.corpus import stopwords
+import nltk
+from nltk.corpus import stopwords
 # from nltk.stem import wordnet
 #from nltk.stem import WordNetLemmatizer
 #from nltk.tokenize import ToktokTokenizer
@@ -21,6 +17,10 @@ import re
 import sklearn 
 # import joblib
 from joblib import load
+#nltk.download('punkt')
+# nltk.download('wordnet')
+nltk.download('stopwords')
+#nltk.download('all') 
 
 app = Flask(__name__)
 
@@ -47,10 +47,9 @@ def text_cleaner(x, lang="english"):
     # x = nltk.tokenize.word_tokenize(x)
     x = x.split(' ')
     # List of stop words in select language from NLTK
-    # stop_words = stopwords.words(lang)
+    stop_words = list(set(stopwords.words(lang)))
     # Remove stop words
-    # x = [word for word in x if word not in stop_words 
-    #     and len(word)>2]
+    x = [word for word in x if word not in stop_words and len(word)>2]
     # Lemmatizer
     # wn = nltk.WordNetLemmatizer()
     # x = [wn.lemmatize(word) for word in x]
