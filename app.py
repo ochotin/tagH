cf022015a716efb8006bba5ddc50fddf59a45554
Olivier Chotin <olivier.chotin@ocia.fr>
2022-07-04 13:56:30 +0200

    Signed-off-by: Olivier Chotin <olivier.chotin@ocia.fr>

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ from nltk.stem import WordNetLemmatizer
 #from nltk.tokenize import ToktokTokenizer
 # from nltk.tokenize import word_tokenize
 from joblib import load
+
 app = Flask(__name__)
 
 
@@ -74,7 +75,7 @@ def form_example():
     # handle the POST request
     if request.method == 'POST':
         Question = request.form.get('Question')
-        Question_clean = text_cleaner(Question, nlp, pos_list, "english")
+        Question_clean = text_cleaner(Question, "english")
         X_tfidf = vectorizer.transform([Question_clean])
         predict = model.predict(X_tfidf)
         tags_prediction = multilabel_binarizer.inverse_transform(predict)
